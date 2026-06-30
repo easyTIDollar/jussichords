4099ac956bb95f84112ddf508de01284fcb4f792
yeson <64787266+easyTIDollar@users.noreply.github.com>
2026-06-30 23:45:29 +0800

    Fix release workflow for APK build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.jussicodes.music"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "2.0.3"
+        versionCode = 3
+        versionName = "2.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
